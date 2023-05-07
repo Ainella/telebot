07526aa8bb120b9c567f8f2aa5102d69cdebb26c
Ainella <120683313+Ainella@users.noreply.github.com>
2023-05-07 22:28:48 +0200

    Update common.py

--- common.py
+++ common.py
@@ -3,10 +3,12 @@ from telebot import types
 from payment import payment_start
 import payment
 import donation2
+import requests
+from bs4 import BeautifulSoup
 #from save import API_KEY
-bot=telebot.TeleBot("6203741928:AAEQ9FykRyvZyEdJBAAnml8JAwXZfkVV4Mg", parse_mode=None)
+bot=telebot.TeleBot("6211666522:AAH4tI0D1xD0qIAi3Yv_yho0EJ7s8t-QSec", parse_mode=None)
 import telebot
-
+supported = ['Alice\n', 'Betty\n', 'Blaze\n', 'Kacper\n', 'Kacper\n', 'Iga\n', 'Nina\n', 'Dorian\n', 'Alicja\n', 'Emilia\n', '/dog']
 # This dictionary will store the user's full names
 user_names = {}
 
@@ -58,40 +60,14 @@ def handle_callback_query(call):
     elif color == 'Walking pets':
         # Send a love sticker
         bot.send_photo(call.message.chat.id, 'https://www.shutterstock.com/image-photo/portrait-cat-dog-looking-camera-260nw-1929069635.jpg')
-        yes_button = types.InlineKeyboardButton('cat', callback_data='cat')
-        no_button = types.InlineKeyboardButton('Dog', callback_data='dog')
-        yes_no_keyboard = types.InlineKeyboardMarkup().add(yes_button, no_button)
         
         # Send a message with the inline keyboards
 
-        bot.send_message(call.message.chat.id, 'Looking for a reliable and trustworthy pet walking service? Look no further than our company! We specialize in providing exceptional care and attention to your furry friends, ensuring they get the exercise and attention they need.\n Our experienced team of pet walkers are fully trained and equipped to handle all types of dogs, big or small, and will work with you to create a personalized walking schedule that fits your needs and your pets needs.\n \n We pride ourselves on providing a safe and secure environment for your pets, and our team is fully insured and bonded for your peace of mind. Plus, we offer competitive rates and flexible scheduling options to make it easy and convenient for you to keep your pet healthy and happy. \n \n So why wait? Contact us today to schedule your pets first walk with us and experience the difference that our expert pet walking service can make! \n \n WHICH PET DO YOU HAVE?', reply_markup=yes_no_keyboard)
+        bot.send_message(call.message.chat.id, 'Welcome to see information about your dog. please click\n /cat\n /dog\n to see supported names /supported_names')
         
-    elif color=='cat':
-        bot.send_photo(call.message.chat.id, 'https://www.jacksongalaxy.com/wp-content/uploads/2018/08/walk.jpg')
-        Mon_button = types.InlineKeyboardButton('Monday', callback_data='Monday')
-        Tue_button = types.InlineKeyboardButton('Tuesday', callback_data='Tuesday')
-        Wed_button = types.InlineKeyboardButton('Wednsday', callback_data='Wednsday')
-        Thu_button = types.InlineKeyboardButton('Thursday', callback_data='Thursday')
-        Fri_button = types.InlineKeyboardButton('Friday', callback_data='Friday')
-        Sat_button = types.InlineKeyboardButton('Saturday', callback_data='Saturday')
-        Sun_button = types.InlineKeyboardButton('Sunday', callback_data='Sunday')
     
-        week_keyboard = types.InlineKeyboardMarkup().add(Mon_button, Tue_button, Wed_button, Thu_button, Fri_button, Sat_button, Sun_button)        
-        # Send a message with the inline keyboards
-        bot.send_message(call.message.chat.id, 'Choose a date?', reply_markup=week_keyboard)
-    
-    elif color=='dog':
-        bot.send_photo(call.message.chat.id, 'https://www.cesarsway.com/wp-content/uploads/2015/06/6-tips-for-mastering-the-dog-walk.jpg')
-        Mon_button = types.InlineKeyboardButton('Monday', callback_data='Monday')
-        Tue_button = types.InlineKeyboardButton('Tuesday', callback_data='Tuesday')
-        Wed_button = types.InlineKeyboardButton('Wednesday', callback_data='Wednesday')
-        Thu_button = types.InlineKeyboardButton('Thursday', callback_data='Thursday')
-        Fri_button = types.InlineKeyboardButton('Friday', callback_data='Friday')
-        Sat_button = types.InlineKeyboardButton('Saturday', callback_data='Saturday')
-        Sun_button = types.InlineKeyboardButton('Sunday', callback_data='Sunday')
-        week_keyboard = types.InlineKeyboardMarkup().add(Mon_button, Tue_button, Wed_button, Thu_button, Fri_button, Sat_button, Sun_button)        
-        # Send a message with the inline keyboards
-        bot.send_message(call.message.chat.id, 'Choose a date?', reply_markup=week_keyboard)
+
+
     elif color=='Electricity payment':    
         bot.send_photo(call.message.chat.id, 'https://www.jaipurstuff.com/wp-content/uploads/2022/09/pay-advance-electricity-bill.jpg')
         keyboard = types.InlineKeyboardMarkup()
@@ -103,77 +79,7 @@ def handle_callback_query(call):
     elif color == '/payment_start':
         bot.send_message(call.message.chat.id, 'Please, enter your fullname?') 
         bot.register_next_step_handler(call.message, payment.get_response2)
-    elif color=='Monday':
-        
-        
-        one_button = types.InlineKeyboardButton('13-15', callback_data='13-15')
-        two_button = types.InlineKeyboardButton('15-17', callback_data='15-17')
-        thr_button = types.InlineKeyboardButton('17-19', callback_data='17-19')
-        four_button = types.InlineKeyboardButton('19-21', callback_data='19-21')
-        week_keyboard = types.InlineKeyboardMarkup().add(one_button, two_button, thr_button, four_button)
-        
-        
-        
-        bot.send_message(call.message.chat.id, 'Choose a time?', reply_markup=week_keyboard)
-    elif color=='Tuesday':
-        markup = types.ReplyKeyboardMarkup(row_width=1)
-        A_button = types.KeyboardButton('13:00-15:00')
-        B_button = types.KeyboardButton('15:00-17:00')
-        C_button = types.KeyboardButton('17:00-19:00')    
-        D_button = types.KeyboardButton('19:00-21:00')
-        
-        markup.add(A_button, B_button, C_button, D_button)
-        
-        bot.send_message(call.message.chat.id, 'choose a time?', reply_markup=markup)  
-    elif color=='Wednsday':
-        markup = types.ReplyKeyboardMarkup(row_width=1)
-        A_button = types.KeyboardButton('13:00-15:00')
-        B_button = types.KeyboardButton('15:00-17:00')
-        C_button = types.KeyboardButton('17:00-19:00')    
-        D_button = types.KeyboardButton('19:00-21:00')
-        
-        markup.add(A_button, B_button, C_button, D_button)
-        
-        bot.send_message(call.message.chat.id, 'choose a time?', reply_markup=markup)  
-    elif color=='Thursday':
-        markup = types.ReplyKeyboardMarkup(row_width=1)
-        A_button = types.KeyboardButton('13:00-15:00')
-        B_button = types.KeyboardButton('15:00-17:00')
-        C_button = types.KeyboardButton('17:00-19:00')    
-        D_button = types.KeyboardButton('19:00-21:00')
-        
-        markup.add(A_button, B_button, C_button, D_button)
-        
-        bot.send_message(call.message.chat.id, 'choose a time?', reply_markup=markup)
-    elif color=='Friday':
-        markup = types.ReplyKeyboardMarkup(row_width=1)
-        A_button = types.KeyboardButton('13:00-15:00')
-        B_button = types.KeyboardButton('15:00-17:00')
-        C_button = types.KeyboardButton('17:00-19:00')    
-        D_button = types.KeyboardButton('19:00-21:00')
-        
-        markup.add(A_button, B_button, C_button, D_button)
-        
-        bot.send_message(call.message.chat.id, 'choose a time?', reply_markup=markup)
-    elif color=='Saturday':
-        markup = types.ReplyKeyboardMarkup(row_width=1)
-        A_button = types.KeyboardButton('13:00-15:00')
-        B_button = types.KeyboardButton('15:00-17:00')
-        C_button = types.KeyboardButton('17:00-19:00')    
-        D_button = types.KeyboardButton('19:00-21:00')
-        
-        markup.add(A_button, B_button, C_button, D_button)
-        
-        bot.send_message(call.message.chat.id, 'choose a time?', reply_markup=markup)
-    elif color=='Sunday':
-        markup = types.ReplyKeyboardMarkup(row_width=1)
-        A_button = types.KeyboardButton('13:00-15:00')
-        B_button = types.KeyboardButton('15:00-17:00')
-        C_button = types.KeyboardButton('17:00-19:00')    
-        D_button = types.KeyboardButton('19:00-21:00')
-        
-        markup.add(A_button, B_button, C_button, D_button)
-     
+    
      
     #Ainella's code, please do not remove   
     elif color == 'Card':
@@ -189,8 +95,36 @@ def handle_callback_query(call):
    
    
     else:
-        bot.send_message(call.message.chat.id, f"We are delighted to confirm your appointment for pet walking service with our company. Our team is thrilled to have the opportunity to take care of your beloved pet and ensure that they get the exercise and attention they need. \n \n Your appointment is scheduled for [date] at {color}. Our pet walker will arrive at your specified location, equipped with all the necessary tools and equipment to make sure that your pet has a safe and enjoyable walk. \n \n Our team takes great pride in providing exceptional pet walking services, and we are confident that you and your pet will have a wonderful experience with us. Should you have any questions or concerns prior to your appointment, please do not hesitate to contact us. \n \n Thank you for choosing our company for your pet walking needs. We look forward to serving you and your furry friend soon.")
+        bot.send_message(call.message.chat.id, f"Please click /cat or /dog")
+@bot.message_handler(commands=['cat', 'dog'])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome to the dog information bot. Please enter the name of a dog to get its information.")
 
+@bot.message_handler(commands=['supported_names'])
+def show_parse_help(message):
+    bot.send_message(message.from_user.id, f"I am able to find the information only for the following dogs:\n {' '.join(supported)}")
+    
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    # Get the name of the dog from the user input
+    dog_name = message.text.strip().lower()
+    
+    # Parse the website using BeautifulSoup
+    #response = requests.get('https://www.worldometers.info/coronavirus/')
+    #soup = BeautifulSoup(response.text, 'html.parser')
+    
+    with open('Echin.html') as f:
+        soup = BeautifulSoup(f, 'html.parser')
+        
+    # Find the table rows containing the dog's information
+    table = soup.find('table')
+    rows = table.find_all('tr')
+    for row in rows:
+        cols = row.find_all('td')
+        cols = [col.text.strip().lower() for col in cols]
+        if dog_name in cols:
+            # If the dog's name is found, send its information to the user
+            bot.reply_to(message, f"Name: {cols[0]}\nGender: {cols[1]}\nBreed: {cols[2]}\nLast_appointment: {cols[3]}\nBehaviour: {cols[4]}\nLast_payment: {cols[6]}")
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, "Please write /help")      
